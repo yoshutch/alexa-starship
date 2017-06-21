3a2de82ec495753ece1010ea1f0a70948cea8972
Scott Hutchings <yoshutch@gmail.com>
2017-06-21 11:03:40 -0600

    asdf

---
+++ items.js
@@ -0,0 +1,26 @@
+module.exports = {
+	// Weapons
+	basicLaser : {
+		name: "Basic Laser",
+		beam_atk: 10
+	},
+
+	basicMissile : {
+		name: "Basic Missile",
+		missile_atk: 10
+	},
+
+	basicParticleCannon : {
+		name: "Basic Particle Cannon",
+		particle_atk: 10
+	},
+
+	// Shields
+	basicShield : {
+		name: "Basic Shield",
+		beam_def: 1,
+		missile_def: 1,
+		particle_def: 1
+	}
+}
+
